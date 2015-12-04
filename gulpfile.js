1b02009fbe737cc3615619483ec42c6e933be248
RenatoFX <renatoferreiraxavier@gmail.com>
2015-12-04 15:05:39 -0200

    set up handlebars

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,17 @@ var minifyCss = require('gulp-minify-css');
 var sourceMaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var imageMin = require('gulp-imagemin');
+var handlebars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
+
+gulp.task('templates', function(){
+    return gulp.src(['src/templates/**/*.hbs'])
+        .pipe(handlebars())
+        .pipe(rename(function(path){
+            path.extname = '.html'
+        }))
+        .pipe(gulp.dest('./'));
+})
 
 gulp.task('images', function(){
     gulp.src(['src/img/**/*'])
@@ -33,7 +44,7 @@ gulp.task('styles', function(){
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', function(){
+gulp.task('default', ['styles', 'images', 'scripts', 'templates'],function(){
     browserSync.init({
         server: './'
     });
@@ -41,5 +52,6 @@ gulp.task('default', function(){
     gulp.watch('src/styles/**/*.css', ['styles']);
     gulp.watch('src/img/**/*', ['images']);
     gulp.watch('src/scripts/**/*.js', ['scripts']);
+    gulp.watch('src/templates/**/*.hbs', ['templates']);
     gulp.watch('*.html',browserSync.reload);
 });
